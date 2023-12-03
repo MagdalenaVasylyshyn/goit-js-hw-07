ccb88f6d719e166386a41dea29888a80223f6f42
MagdalenaVasylyshyn <141566623+MagdalenaVasylyshyn@users.noreply.github.com>
2023-12-03 16:14:49 +0200

    Second version of seventh homework

--- js/task-04.js
+++ js/task-04.js
@@ -1,3 +1,22 @@
+const loginForm = document.querySelector(".login-form")
+const register = (event) => {
+        event.preventDefault()
+  const emailValue = loginForm.elements.email.value.trim();
+  const passwordValue = loginForm.elements.password.value.trim();
+  if (!emailValue || !passwordValue) {
+    alert("All form fields must be filled in");
+    return;
+  }
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+  console.log(formData);
+  loginForm.reset();
+};
+loginForm.addEventListener("submit", register);
+
+
 // const loginForm = document.getElementById("loginForm");
 // loginForm.addEventListener("submit", function (event) {
 //   event.preventDefault();
--- js/task-05.js
+++ js/task-05.js
@@ -1,5 +1,16 @@
+const changeColorButton = document.querySelector(".change-color")
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColor = () => {
+  const spanNameColor = document.querySelector(".color");
+  const body = document.querySelector("body");
+  body.style.backgroundColor = getRandomHexColor();
+  spanNameColor.textContent = getRandomHexColor();
+};
+
+changeColorButton.addEventListener("click", changeColor);
